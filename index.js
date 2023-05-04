5d7a3836385873e4593f524f957fd7fac268e0ee
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-05-03 20:57:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -84,3 +84,29 @@ let text = `Hello ${username}!<br>
             You currently have ${items} items in your cart which comes to a total of $${total}<br>`
 //console.log(text);
 document.getElementById("myText").innerHTML = text;
+
+/*
+    Shuffle an array
+*/
+
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", " K" ];
+shuffle(cards);
+
+cards.forEach(card => console.log(cards));
+shuffle(cards);
+
+cards.forEach(card => console.log(cards))
+
+function shuffle(array){
+    let currentIndex = array.length;
+
+    while(currentIndex != 0){
+        let randomIndex = Math.floor(Math.random()* array.length)
+        currentIndex -= 1;
+
+        let temp = array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+
+    return array;
+}
